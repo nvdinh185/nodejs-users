c4778378eb35ecfe6718d140928d491dd268c8fc
nvdinh185 <nvdinh0766777123@gmail.com>
2023-06-07 19:28:05 +0700

    initial commit

---
+++ client/js/main.js
@@ -0,0 +1,50 @@
+const table = document.getElementById("table");
+
+async function getData() {
+
+    try {
+        var listUsers = await axios.get('http://localhost:3000/users');
+        listUsers = listUsers.data;
+
+        var htmls = `
+                <table>        
+                    <tr>
+                            <th>Username</th>
+                            <th>Fullname</th>
+                    </tr>`
+
+        for (let us of listUsers) {
+            htmls += `
+                    <tr>
+                            <td>${us.username}</td>
+                            <td>${us.fullname}</td>
+                    </tr>`
+        }
+        htmls += `</table>`
+        table.innerHTML = htmls;
+
+    } catch (err) {
+        console.log('Lỗi ' + err);
+    }
+}
+
+getData();
+
+function getParameterByName(name, url = location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+var msg = getParameterByName('msg');
+
+var msgElement = document.querySelector('#msg');
+msgElement.setAttribute('style', 'color: green; background: yellow');
+if (msg === '1') {
+    msgElement.innerText = 'Đăng ký thành công!';
+} else if (msg === '2') {
+    msgElement.innerText = 'Đăng nhập thành công!';
+}
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const users = [
+    {
+        username: 'dinh',
+        password: '123',
+        fullname: 'Nguyen Van Dinh'
+    }
+]
+
+app.use(express.static(__dirname + '/client'));
+
+app.post('/register', (req, res) => {
+    var user = req.body;
+    var check = false;
+
+    for (const el of users) {
+        if (user.username === el.username) {
+            check = true;
+        }
+    }
+
+    if (!check) {
+        users.push(user);
+        res.status(200).send({ status: 'OK' });
+    } else {
+        res.status(500).send({ status: 'NOK' });
+    }
+})
+
+app.post('/login', (req, res) => {
+    var user = req.body;
+    var check = false;
+
+    for (const el of users) {
+        if (user.username === el.username
+            && user.password === el.password) {
+            check = true;
+        }
+    }
+
+    if (check) {
+        res.status(200).send({ status: 'OK' });
+    } else {
+        res.status(500).send({ status: 'NOK' });
+    }
+})
+
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
