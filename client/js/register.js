b8fd9ba02a0e8c5928b85b756293158543595400
nvdinh185 <nvdinh0766777123@gmail.com>
2024-01-13 20:35:39 +0700

    x

--- client/js/register.js
+++ client/js/register.js
@@ -1,5 +1,31 @@
 var form = $('#register-form');
 
+// Hàm để xử lý khi blur hoặc nhập vào ô input
+function handleBlurInput(input) {
+    var errorElement = input.parent().children()[3];
+    input.blur(function () {
+        if (input.val().trim() === '') {
+            $(errorElement).attr('style', 'color: red; font-style: italic;');
+            $(errorElement).text('Yêu cầu nhập!');
+            input.addClass('invalid');
+        }
+    })
+
+    input.on('input', function () {
+        $(errorElement).attr('style', 'display: none;');
+        input.removeClass('invalid');
+    })
+}
+
+var usernameElement = $("#username");
+var passwordElement = $("#password");
+var fullnameElement = $("#fullname");
+
+handleBlurInput(usernameElement);
+handleBlurInput(passwordElement);
+handleBlurInput(fullnameElement);
+
+// Xử lý submit form
 form.on('submit', async function (e) {
     e.preventDefault();
 
@@ -11,29 +37,53 @@ form.on('submit', async function (e) {
         });
     }
 
-    const formValue = {};
-    formValue["id"] = generateUuid();
-
-    // Lấy dữ liệu nhập vào từ form
-    for (const el of e.target) {
-        if (el.name) {
-            formValue[el.name] = el.value;
+    // Hàm kiểm tra yêu cầu nhập ô input
+    function isRequired(input) {
+        var errorElement = input.parent().children()[3];
+        if (input.val().trim() === '') {
+            $(errorElement).attr('style', 'color: red; font-style: italic;');
+            $(errorElement).text('Yêu cầu nhập!');
+            input.addClass('invalid');
+            return true;
         }
     }
 
+    var check = true;
+    if (isRequired(usernameElement)) {
+        check = false;
+    }
+    if (isRequired(passwordElement)) {
+        check = false;
+    }
+    if (isRequired(fullnameElement)) {
+        check = false;
+    }
 
-    // console.log(formValue);
-    try {
-        await axios({
-            method: "POST",
-            url: 'http://localhost:3000/users',
-            data: formValue
-        });
+    if (check) {
+        const formValue = {};
+        formValue["id"] = generateUuid();
 
-        // chuyển hướng về lại trang index.html
-        location = 'index.html?msg=1';
-    } catch (error) {
-        var errorElement = $('.error');
-        errorElement.html('<p style="color: red; ; font-style: italic">Xảy ra lỗi khi thêm!</p>');
+        // Lấy dữ liệu nhập vào từ form
+        for (const el of e.target) {
+            if (el.name) {
+                formValue[el.name] = el.value;
+            }
+        }
+
+
+        // console.log(formValue);
+        try {
+            await axios({
+                method: "POST",
+                url: 'http://localhost:3000/users',
+                data: formValue
+            });
+
+            // chuyển hướng về lại trang index.html
+            location = 'index.html?msg=1';
+        } catch (error) {
+            var errorElement = $('.error');
+            errorElement.html('<p style="color: red; ; font-style: italic">Xảy ra lỗi khi thêm!</p>');
+        }
     }
 })
